e73ee2b54b3d922d2f9fd7703fe7bd1ac7766350
Precious Adedibu <adedibuprecious@gmail.com>
2022-10-17 21:41:26 +0100

    delete input

--- src/components/ui/form/input.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./index.module.scss";
-
-const Input = ({ ...rest }) => {
-  return <input {...rest} className={styles.input} />;
-};
-
-export default Input;
